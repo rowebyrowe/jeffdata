c13b45a7b6bab3c6c00d0ca4df60e4de78c2a807
Jeffrowetull <jeffrowetull@gmail.com>
2019-05-29 13:57:35 -0700

    modified helper files to be somewhat pep8 compatible

--- dateCutter.py
+++ dateCutter.py
@@ -3,12 +3,14 @@ import pandas as pd
 A little function that will split the date into three
 separate columns and make a new dataframe
 '''
+
+
 def splitter(series):
     dates = []
     for i in series:
         cut = i.split('/')
         dates.append(cut)
-    df = pd.DataFrame(dates, columns=['Month','Day','Year'])
+    df = pd.DataFrame(dates, columns=['Month', 'Day', 'Year'])
     print(df)
 
-splitter(['11/10/9','10/10/10'])
+splitter(['11/10/9', '10/10/10'])
--- nullHelper.py
+++ nullHelper.py
@@ -2,22 +2,23 @@ class NullHelper:
     '''A class to help out your nulls!'''
     import pandas as pd 
 
-    def __init__(self,df):
+    def __init__(self, df):
         self.df = df
 
-    def check_for_values(self,value):
+    def check_for_values(self, value):
         '''let's you input common values that could be nans
         and will tell you how many a column has'''
         columns = self.columns
         if value in self.index:
             for column in columns:
-                print(column,'nulls:',self[self[column]==value].sum(),
-                'percentage of whole:',self[self[column]==value].sum()/self[column].count()*100,'%','\n')
+                print(column, 'nulls:', self[self[column] == value].sum(),
+                      'percentage of whole:', 
+                      self[self[column] == value].sum()/self[column].count()*100, 
+                      '%', '\n')
         else:
             print('No such value found')
 
     def replace_nulls(self, columns, before, after):
         for column in columns:
-            self.column = self.column.replace(before,after)
+            self.column = self.column.replace(before, after)
         return self
-
